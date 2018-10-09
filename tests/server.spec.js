5d1590b5717304b7992d22db358d78ece665c701
Tiana Hayden <tiana.hayden@me.com>
2018-10-09 12:58:49 -0700

    Linked Circle CI to Heroku and added new test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says sup Tiana', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Sup Tiana');
+        done();
+      });
+    });
 })
